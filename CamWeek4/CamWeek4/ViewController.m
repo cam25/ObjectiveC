3d944f29719533da14c0d4c030b87599f2bbb54d
Cameron Mozie <cam25@fullsail.edu>
2013-01-25 14:23:48 -0500

    Conditional setup for onClick of button

--- CamWeek4/CamWeek4/ViewController.m
+++ CamWeek4/CamWeek4/ViewController.m
@@ -25,18 +25,20 @@
     }
     [self.view addSubview:username];
     
-    label2 = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 90.0f, 200.0f, 60.0f)];
+    label2 = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 120.0f, 320.0f, 70.0f)];
     if (label2 != nil) {
         label2.backgroundColor = [UIColor grayColor];
         label2.text= @"Please Enter Username";
-        label2.textAlignment = NSTextAlignmentLeft;
+        label2.textAlignment = NSTextAlignmentCenter;
         label2.textColor = [UIColor blueColor];
+       
     }
     [self.view addSubview:label2];
     
     textField = [[UITextField alloc] initWithFrame:CGRectMake(90.0f, 10.0f, 200.0f, 30.0f)];
     if (textField != nil) {
         textField.borderStyle = UITextBorderStyleRoundedRect;
+       
         [self.view addSubview:textField];
     }
     
@@ -53,12 +55,19 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
-
+//on button click
 - (void)onClick
 {
     NSString *userText = [textField text];
     
-}
+    //button condition
+    if (userText.length < 1) {//if userText is less than 1 username cannot be empty
+        label2.text =  @"Username cannot be empty";
+        
+    }else if(userText.length > 1)//else if userText entered is greater than 1 store username
+        label2.text = @"User: username has been logged in";
+    NSLog(@"%@", userText);
+    }
 
 - (void)didReceiveMemoryWarning
 {
