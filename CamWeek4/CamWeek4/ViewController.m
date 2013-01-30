9ee15fe6e72dc3283635e91ed86702e94f949314
Cameron Mozie <cam25@fullsail.edu>
2013-01-30 14:58:02 -0500

    Commits for week 4

--- CamWeek4/CamWeek4/ViewController.m
+++ CamWeek4/CamWeek4/ViewController.m
@@ -18,7 +18,7 @@
 
 - (void)viewDidLoad
 {
-self.view.backgroundColor = [UIColor lightGrayColor];
+    self.view.backgroundColor = [UIColor lightGrayColor];
     
     username = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 15.0f, 80.0f, 20.0f)];
     if (username != nil) {
@@ -35,14 +35,14 @@ self.view.backgroundColor = [UIColor lightGrayColor];
         label2.text= @"Please Enter Username";
         label2.textAlignment = NSTextAlignmentCenter;
         label2.textColor = [UIColor blueColor];
-       
+        
     }
     
     
     textField = [[UITextField alloc] initWithFrame:CGRectMake(90.0f, 10.0f, 200.0f, 30.0f)];
     if (textField != nil) {
         textField.borderStyle = UITextBorderStyleRoundedRect;
-       
+        
         [self.view addSubview:textField];
     }
     
@@ -54,7 +54,7 @@ self.view.backgroundColor = [UIColor lightGrayColor];
         [button addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
         button.tag =  BUTTON_ZERO;
         [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-      
+        
         
         [self.view addSubview:button];
     }
@@ -83,65 +83,83 @@ self.view.backgroundColor = [UIColor lightGrayColor];
         label3.textAlignment = NSTextAlignmentLeft;
         label3.numberOfLines = 2;
         label3.textColor = [UIColor blueColor];
-
+        
     }
-   
+   //sets a tap gesture which 
+       
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
+
+
+
 //on button click
 - (void)onClick:(UIButton*)button
 {
-    NSString *userText = [textField text];
+    
+    switch (BUTTON_ZERO) {
+        case 0:
+            [textField resignFirstResponder];
+            break;
+            
+        default:
+            break;
+    }
+    
     if(button.tag == BUTTON_ZERO)
     {
+        NSString *userText = [textField text];
         [self.view addSubview:label2];//displays label 2 if login button clicked
         
-    //button condition
-    if (userText.length < 1) {//if userText is less than 1 username cannot be empty
-        label2.text =  @"Username cannot be empty";
-        
-    }else if(userText.length > 1)//else if userText entered is greater than 1 store username
-        label2.text =[[NSString alloc] initWithFormat: @"User: %@ has been logged in!", userText];
-    NSLog(@"%@", userText);
-    
+        //button condition
+        if (userText.length < 1)//if userText is less than 1 username cannot be empty
+            
+        {
+            label2.text =  @"Username cannot be empty";
+            
+        } else if (userText.length > 1)//else if userText entered is greater than 1 store username
+            
+        {
+            label2.text =[[NSString alloc] initWithFormat: @"User: %@ has been logged in!", userText];
+            NSLog(@"%@", userText);
+            
+        }
     }
-    if (button.tag == BUTTON_ONE) {
+    
+    else if (button.tag == BUTTON_ONE) {
         
         NSDate *date = [NSDate date];
         NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
         
-            //setdate formate
-            [dateFormatter setDateFormat:@" MMMM dd,yyyy hh:mm:ss a zzzz"];
-            NSString *showString = [dateFormatter stringFromDate:date];//date formatted to a string for passing into alert
+        //setdate formate
+        [dateFormatter setDateFormat:@" MMMM dd,yyyy hh:mm:ss a zzzz"];
+        NSString *showString = [dateFormatter stringFromDate:date];//date formatted to a string for passing into alert
         
-        //alert 
+        //alert
         UIAlertView *showDateAlert = [[UIAlertView alloc] initWithTitle:@"Date" message:showString delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
         
         if (showDateAlert != nil) {
             [showDateAlert show];
         }
     }
-if (button.tag == BUTTON_TWO) {//if info button clicked display message
-    [self.view addSubview:label3];
-    label3.text = @"This application was created by: Cameron Mozie";
-    
-    //toggel for info button if hidden show if shown and info clicked hide label
-    if ([label3 isHidden]){
+    else if (button.tag == BUTTON_TWO) {//if info button clicked display message
+        [self.view addSubview:label3];
+        label3.text = @"This application was created by: Cameron Mozie";
         
-        [label3 setHidden:NO];
+        //toggel for info button if hidden show if shown and info clicked hide label
+        if ([label3 isHidden]){
+            
+            [label3 setHidden:NO];
+            
+        }
+        else if ([label3 isHidden]==NO)
+            [label3 setHidden:YES];
         
-    } else if ([label3 isHidden] == NO)
-        [label3 setHidden:YES];
-    
+    }
     
 }
-    
-    
-  
-}
 
-
+//keyboard function
 
 - (void)didReceiveMemoryWarning
 {
